a69f979e2f40a54f19ff88ce4bed50d9c3f045bc
Dormant512 <mikegrig@inbox.ru>
2022-05-20 17:12:03 +0300

    Blog updates

--- _authors/dormant.md
+++
@@ -1,7 +0,0 @@
----
-short_name: dormant
-name: Dormant 512
-position: Tea enjoyer
----
-
-Dormant 512 ищет себя на чайном пути и параллельно с этим перемещается из химии в ИТ.
---
+++ _site/assets/js/script.js
@@ -0,0 +1,21 @@
+function resizeContent() {
+    let emFrameWidth = window.innerWidth / 16;
+    // let emCardWidth = document.getElementsByClassName("post-card")[0] / 16;
+    let num = Math.floor(emFrameWidth / 24);
+
+    if ((num * 24 + 1) / num > 0.8) {
+        num -= 1;
+    }
+
+    if (num < 1) {
+        num = 1;
+    }
+
+    let res = num * 24.2 + 1;
+
+    let container = document.getElementById("content-box");
+    
+    container.style.width = String(res) + "em";
+
+    // console.log(`${emFrameWidth} into ${res}`);
+}
